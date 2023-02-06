37dea469d0f11dbd1b1b3a5e6b032eb064261c83
Qoosim <ayinde.abdulghaniyy@gmail.com>
2023-02-06 10:52:43 +0100

    Create Product page with all the products populate on the products page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { useSelector } from 'react-redux';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from 'pages/layout';
 import Dashboard from 'pages/dashboard';
+import Products from 'pages/products';
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -20,6 +21,7 @@ function App() {
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path='/products' element={<Products />} />
             </Route>
           </Routes>
         </ThemeProvider>
