d9499f2a7e29b8030da4f4eb97662fd3f7edf47d
Qoosim <ayinde.abdulghaniyy@gmail.com>
2023-02-07 17:49:05 +0100

    Create Transactions page

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,23 +1,36 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
-    reducerPath: "adminApi",
-    tagTypes: ["User", "Products", "Customers"],
-    endpoints: (build) => ({
-        getUser: build.query({
-            query: (id) => `general/user/${id}`,
-            providesTags: ["User"]
-        }),
-        getProducts: build.query({
-            query: () => 'client/products',
-            providesTags: ["Products"]
-        }),
-        getCustomers: build.query({
-            query: () => 'client/customers',
-            providesTags: ["Customers"]
-        })
-    })
-})
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  reducerPath: "adminApi",
+  tagTypes: ["User", "Products", "Customers", "Transactions"],
+  endpoints: (build) => ({
+    getUser: build.query({
+      query: (id) => `general/user/${id}`,
+      providesTags: ["User"],
+    }),
+    getProducts: build.query({
+      query: () => "client/products",
+      providesTags: ["Products"],
+    }),
+    getCustomers: build.query({
+      query: () => "client/customers",
+      providesTags: ["Customers"],
+    }),
+    getTransactions: build.query({
+      query: ({ page, pageSize, sort, search }) => ({
+        url: "client/transactions",
+        method: "GET",
+        params: { page, pageSize, sort, search },
+      }),
+      providesTags: ["Transactions"],
+    }),
+  }),
+});
 
-export const { useGetUserQuery, useGetProductsQuery, useGetCustomersQuery } = api;
+export const {
+  useGetUserQuery,
+  useGetProductsQuery,
+  useGetCustomersQuery,
+  useGetTransactionsQuery,
+} = api;
